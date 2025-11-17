875679217f348f3ee52b451a1a46575a17628ed0
Filip <isberget23@gmail.com>
2025-11-17 14:46:00 +0100

    Tester och grejer

--- src/test/java/iths/CalculatorTest.java
+++ src/test/java/iths/CalculatorTest.java
@@ -4,12 +4,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+    public int a;
+    public int b;
+    int count;
+    public double c;
+    public double d;
 
     @Test
     void testAddOnePlusTwoEqualsThree() {
-        Calculator cal = new Calculator();
+        // Calculator cal = new Calculator();
 
-        assertEquals(3, cal.add(1, 2));
+        assertEquals(3, Calculator.add(1, 2));
+
+    }
+
+    @Test
+    void testDivideTenWithTwoEqualsFive() {
+        // Calculator calTest = new Calculator();
+        assertEquals(5, Calculator.divide(10, 2));
+    }
+
+    @Test
+
+    void testDivisionByZeroEqualsFalse() {
+
+        IllegalArgumentException ex = assertThrowsExactly(IllegalArgumentException.class,
+                () -> Calculator.divide(5, 0));
+        assertEquals("Division by zero is not allowed", ex.getMessage());
     }
 
 }
